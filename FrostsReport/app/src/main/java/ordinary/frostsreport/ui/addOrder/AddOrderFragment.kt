77bc1626700c0a53781d2ec7aae500982e2070ab
Ahmedjanov Shahzod <80168982+Ahmedjanov@users.noreply.github.com>
2022-07-02 22:11:33 +0600

    change galary item to addOrder

--- FrostsReport/app/src/main/java/ordinary/frostsreport/ui/addOrder/AddOrderFragment.kt
+++ FrostsReport/app/src/main/java/ordinary/frostsreport/ui/addOrder/AddOrderFragment.kt
@@ -1,4 +1,4 @@
-package ordinary.frostsreport.ui.gallery
+package ordinary.frostsreport.ui.addOrder
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import ordinary.frostsreport.databinding.FragmentGalleryBinding
+import ordinary.frostsreport.databinding.FragmentAddOrderBinding
 
-class GalleryFragment : Fragment() {
+class AddOrderFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentAddOrderBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val addOrderViewModel =
+            ViewModelProvider(this).get(AddOrderViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentAddOrderBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        addOrderViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
