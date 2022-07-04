ef7665c7f53cf99339632a722169a973ec4ecdc1
Ahmedjanov Shahzod <80168982+Ahmedjanov@users.noreply.github.com>
2022-07-04 17:02:44 +0600

    realease templates function for add button

--- FrostsReport/app/src/main/java/ordinary/frostsreport/MainActivity.kt
+++ FrostsReport/app/src/main/java/ordinary/frostsreport/MainActivity.kt
@@ -2,6 +2,7 @@ package ordinary.frostsreport
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -26,10 +27,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
+        /*binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -53,5 +54,20 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun onClickAddClient(view:View){
+        Snackbar.make(view, "Add client", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+
+    fun onClickAddOrder(view:View){
+        Snackbar.make(view, "Add order", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+
+    fun onClickAddProduct(view:View){
+        Snackbar.make(view, "Add product", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
     }
 }
