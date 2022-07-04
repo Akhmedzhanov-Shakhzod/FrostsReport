d6a94f3779d50b83c4e7f76f202deeac7c25736b
Ahmedjanov Shahzod <80168982+Ahmedjanov@users.noreply.github.com>
2022-07-04 19:20:46 +0600

    layout addClient and addProduct fragments

--- FrostsReport/app/src/main/java/ordinary/frostsreport/MainActivity.kt
+++ FrostsReport/app/src/main/java/ordinary/frostsreport/MainActivity.kt
@@ -3,7 +3,6 @@ package ordinary.frostsreport
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
