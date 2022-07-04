20990bbddf8448af787fca67aa342f9a0b1c8f6d
Ahmedjanov Shahzod <80168982+Ahmedjanov@users.noreply.github.com>
2022-07-04 19:22:35 +0600

    amend

--- FrostsReport/app/src/main/java/ordinary/frostsreport/ui/addOrder/AddOrderFragment.kt
+++ FrostsReport/app/src/main/java/ordinary/frostsreport/ui/addOrder/AddOrderFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import ordinary.frostsreport.databinding.FragmentAddOrderBinding
 
 class AddOrderFragment : Fragment() {
@@ -22,16 +20,10 @@ class AddOrderFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val addOrderViewModel =
-            ViewModelProvider(this).get(AddOrderViewModel::class.java)
 
         _binding = FragmentAddOrderBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAddOrder
-            addOrderViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
